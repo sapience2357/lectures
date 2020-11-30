2f70d4ab70f80d3bc55693be5f99b38cf089cedc
sapience2357 <bestofbest99@empas.com>
2020-11-30 11:45:45 +0900

    Update shop_comment.py

--- web/shop_comment.py
+++ web/shop_comment.py
@@ -6,32 +6,50 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.firefox.options import Options
 
 from fake_useragent import UserAgent
 import time
 
 # 브라우저의 옵션을 사용하는 방법
-options = Options()
-options.add_argument("--headless")
+# options = Options()
+options = webdriver.ChromeOptions()
+options.add_argument("-headless")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-browser = webdriver.Chrome("D:\IJH\chromedriver.exe")
-browser.get("http://itempage3.auction.co.kr/DetailView.aspx?itemno=B674702537#vip_tab_comment")
+# browser = webdriver.Firefox(executable_path="./geckodrer", options=options)
+browser = webdriver.Chrome(executable_path= './chromedriver', options=options)
 
-try:
-    # soupDocument = bs(response.text,"html.parser")
-    WebDriverWait(browser, 10).\
-        until(EC.presence_of_element_located(
-            (By.CSS_SELECTOR, 'li#tap_moving_2')))
+response = req.get("http://corners.auction.co.kr/AllKill/AllDay.aspx")
+soupDocument = bs(response.text, "html.parser")
+itemList = soupDocument.select("ul.item_list div.inner > a")
 
-    review = browser.find_element_by_css_selector('li#tap_moving_2 a')
-    review.click()
+for item in itemList:
 
-except Exception as e:
-    print(e)
-    # browser.quit()
+    try:
+        browser.get(item['href'])
+        WebDriverWait(browser, 10).\
+            until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, 'li#tap_moving_2')))
 
-   
-# browser.quit()
+        review = browser.find_element_by_css_selector('li#tap_moving_2 a')
+        review.click()
+
+    except Exception as e:
+        print(e)
+        browser.quit()
+
+
+    soupHTML = bs(browser.page_source, "html.parser")
+    commentList = soupHTML.select("ul.list__review li.list-item div.box__review-text > p")
+    
+    for comment in commentList:
+        print(comment.get_text())
+        
+    time.sleep(10)
+
+# with open('./download/shop_by_html_source.html', 'w') as file:
+    # file.write(soupHTML.prettify())
+    
+browser.quit()
